7b1816518ed195a568df91f1f65fea41c8887abc
Yuting Lai <yutingkevinlai@g.ucla.edu>
2020-12-29 21:29:16 -0800

    add multi threading

--- option_selecting.py
+++ option_selecting.py
@@ -7,46 +7,43 @@ from get_all_tickers import get_tickers as gt
 from get_all_tickers.get_tickers import Region
 import os.path
 from os import path
+import threading, multiprocessing
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import itertools
+from itertools import zip_longest
+import concurrent.futures
+from time import time
 
-def GetInfo(udlying, day, month, year, stockprice, offset):
-    u = Call(udlying, d=day, m=month, y=year, strike=stockprice+offset)
-    price = u.price
-    IV = u.implied_volatility()
-    v = u.vega()
-    g = u.gamma()
-    strike = u.strike
-    return (IV,v,g,price,strike)
+optionable_list = []
+filtered_list = []
+min_price = 0
+max_price = 10000
 
-def moves(DTern,DTE,IV,IVn,v,g,price_move,offset):
-    IV_AE = ((DTern/(DTern+DTE))*IV**2 + (DTE/(DTern+DTE))*IVn**2)**0.5
-    vega_change = v*(IV_AE-IV)*100
-    gamma_change = 0.5*g*(price_move+offset)**2
-    return (IV_AE,vega_change,gamma_change)
+def find_optionable_stocks(udlying):
+    g = options.get_expiration_dates(udlying)
+    if len(g) != 0:
+        optionable_list.append(udlying)
+        s = Stock(udlying)
+        if s.price > min_price and s.price < max_price:
+            filtered_list.append(udlying)
 
 def get_tickers():
     list_of_tickers = gt.get_tickers_by_region(Region.NORTH_AMERICA)
-    print(list_of_tickers[3])
     print(len(list_of_tickers))
-    optionable_list = []
-    filtered_list = []
-    minPrice = 20
-    maxPrice = 50
-    for stock_idx in range(1,20):
-        udlying = list_of_tickers[stock_idx]
-        print(stock_idx, udlying)
-        g = options.get_expiration_dates(udlying)
-        if len(g) != 0:
-            optionable_list.append(udlying)
-            s = Stock(list_of_tickers[stock_idx])
-            if s.price > minPrice and s.price < maxPrice:
-                filtered_list.append(list_of_tickers[stock_idx])
+    processes = []
+    start = time()
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        for udlying in list_of_tickers:
+            processes.append(executor.submit(find_optionable_stocks, udlying))
     print(optionable_list)
+    print(filtered_list)
     with open('optionable_list.txt', 'w') as f:
         for item in optionable_list:
             f.write("%s\n" % item)
     # print(len(optionable_list))
     # print(filtered_list)
     # print(len(filtered_list))
+    print(f'Time taken: {time() - start}')
     return optionable_list, filtered_list
 
 def get_volatility(ticker="AAPL"):
